5bdb50e062b5a2c25a57c405f289c5f86fc8d046
DaeHo Kim <zzangisdaeho@naver.com>
2022-04-25 03:54:47 +0900

    datasource test

--- src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -2,7 +2,8 @@ package hello.jdbc.connection;
 
 public abstract class ConnectionConst {
 
-    public static final String URL = "jdbc:h2:tcp://localhost/~/study/h2/data/test";
+//    public static final String URL = "jdbc:h2:tcp://localhost/~/study/h2/data/test"; // company
+    public static final String URL = "jdbc:h2:tcp://localhost//E:/h2/spring-db"; // home
     public static final String USERNAME = "sa";
     public static final String PASSWORD = "";
 }
