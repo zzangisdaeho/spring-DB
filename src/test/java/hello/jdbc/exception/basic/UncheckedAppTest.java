76a9f18466a241c4131f5464dd5dd945687170c1
daeho kim <daeho.kim@softwareinlife.com>
2022-04-28 19:57:13 +0900

    exception test

---
+++ src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -0,0 +1,84 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+public class UncheckedAppTest {
+    @Test
+    void unchecked() {
+        Controller controller = new Controller();
+        assertThatThrownBy(controller::request)
+                .isInstanceOf(Exception.class);
+
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            //e.printStackTrace();
+            log.info("print ex : ", e);
+        }
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call() {
+            throw new RuntimeConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        private void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class RuntimeConnectException extends RuntimeException {
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException() {
+        }
+
+        // Throwable을 받아서 생성하면, stacktrace로 이전 예외의 상황도 출력이 가능하다.
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
